7930c22f02c7c375225486ed1e7fccfc7fb159d0
Elsa S <elsasalinas2388@gmail.com>
2025-08-15 13:06:51 -0400

    did all the logic code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -79,7 +79,7 @@ subMenuEl.style.top = '0';
 let topMenuLinks= topMenuEl.querySelectorAll ('a'); //  grab inside big box 
 
 //Attach a delegated 'click' event listener to topMenuEl.
-topMenuEl.addEventListener.('click', function (event);
+topMenuEl.addEventListener.'click', function (event);
 
 //The first line of code of the event listener function should call the event object's preventDefault() method.
 event.preventDefault();  // **stop the link from navigating away***
@@ -112,6 +112,87 @@ console.log(event.target.tagName);   //** logthe link text */
   event.target.classList.add('active');
 });
 
+// PARTFIVE ADDING SUBMENU
+//Within the event listener, if the clicked <a> element does not yet have a class of "active" (it was inactive when clicked):
+//If the clicked <a> element's "link" object within menuLinks has a subLinks property (all do, except for the "link" object for ABOUT), set the CSS top property of subMenuEl to 100%.
+//Otherwise, set the CSS top property of subMenuEl to 0.
+// // //////////////////////////////////////////////////
+
+// a) Save the text from the link that was clicked into a variable.
+//    "event.target" means: the exact thing the user clicked.
+//    ".textContent" means: the words inside that link.
+//    ".toLowerCase()" makes it all lowercase so it matches the way our menuLinks data is written.
+const linkText = event.target.textContent.toLowerCase();
+
+// b) Look through our menuLinks array to find the object that matches the linkText we just saved.
+//    ".find()" goes through each object in the array until it finds one where the text matches linkText.
+const clickedLinkObj = menuLinks.find(function(link) {
+  return link.text === linkText;
+});
+
+// c) Check TWO things before showing the submenu:
+//    - clicked link have the "active" class = Meaning it was just clicked and is turned on
+//    -  "subLinks" property = Meaning it has a submenu list
+if (event.target.classList.contains('active') && clickedLinkObj.subLinks) {
+  
+  // d) If BOTH are true → move the submenu down into view.
+  subMenuEl.style.top = '100%';
+  
+} else {
+  
+  // e) If either one is false → hide the submenu.
+  //    "style.top = '0'" moves it up so it disappears from view.
+  subMenuEl.style.top = '0';
+}
+////////////////////*********** */
+// Clear the current contents of subMenuEl.
+subMenuEl.innerHTML ='';
+
+//Iterate over the subLinks array, passed as an argument, and for each "link" object:
+subLinks.forEach(function(link)){
+
+//Create an <a> element.
+const aEL =document.createElement('a');
+
+//Add an href attribute to the <a>, with the value set by the href property of the "link" object.
+aEL.setAttribute('href', link.href);
+
+//Set the element's content to the value of the text property of the "link" object.
+aEL.textContent = link.text;
+
+//Append the new element to the subMenuEl.
+subMenuEl.appendChild(aEL);}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//Attach a delegated 'click' event listener to subMenuEl.
+subMenuEl.addEventListener('click', function (event));
+
+//The first line of code of the event listener function should call the event object's preventDefault() method.
+event.preventDefault();
+
+//The second line of code within the function should immediately return if the element clicked was not an <a> element.
+if (event.target.tagName ! == 'A)
+    return;
+
+//Log the content of the <a> to verify the handler is working.
+console.log('subMenu clicked:' , event.target.textContent);
+
+//Next, the event listener should set the CSS top property of subMenuEl to 0.
+subMenuEl.style.top = '0' ;
+
+//Remove the active class from each <a> element in topMenuLinks.
+topMenuLinks.forEach(a => a.classList.remove('active'));
+
+//Update the contents of mainEl, within an <h1>, to the contents of the <a> element clicked within subMenuEl.
+const clickedText = event.target.textContent.trim();
+const h1 = mainEl.querySelector ('h1');
+
+//If the ABOUT link is clicked, an <h1>About</h1> should be displayed.
+if (clickedText.toLowerCase() === 'about'){
+    h1.textContent = 'About';
+} else {
+    h1.textContent = clickedText
+});
 
 
 
